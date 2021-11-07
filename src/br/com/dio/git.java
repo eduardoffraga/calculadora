ea8027176bb9a35c829cfc37336df6dcc7df0a9a
eduardoffraga <edu_fraga68@hotmail.com>
2021-11-07 11:21:20 -0300

    corrigido erros

---
+++ src/br/com/dio/git.java
@@ -0,0 +1,29 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class git {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		Livro livro = new Livro();
+		
+		
+		System.out.println(gato);
+		
+		System.out.println(livro);
+		
+		
+		
+		
+	}
+	
+public class livro {
+	
+	public String nome;
+	public Integer numPaginas;
+	
+}
+
+}
